a003e0ea012d9150a09401aad9771f62caed05b0
AdrienDutertre <adriendutertre89@gmail.com>
2015-10-25 11:32:38 +0100

    Modifs petites erreurs ou warnings et un peu de mise en forme

--- features-creation/featuresCreator.py
+++ features-creation/featuresCreator.py
@@ -6,72 +6,76 @@ import numpy as np
 import extendedSysPath
 from config import Config as conf
 
+
 class featuresExtractor():
 
-	# def __init__()
+	def __init__(self) : 
+
+		self.basicPlayerFeatures = ['age','height','weight','plays','rank','turnedpro','imc']
+		self.basicEventFeatures = ['surface','category','num_of_sets','round','event_week','event_year']
+
 
 	def readData(self):
+		
 		self.dataDF = pd.read_csv(conf.preProcessedMatchsFilePath)
 		return self
 
+	
 	def createBasicFeatures(self):
 		
-		# clean rounds
+		# Uniformisation des rounds puis codage sous forme du nombre de matchs à remporter pour gagner le titre
 		self.dataDF['round'] = self.dataDF['round'].replace(['Round Robin','Semi-Finals','Finals'],['Q','S','F'])
 		self.dataDF['round'] = self.dataDF['round'].replace(['R128','R64','R32','R16','Q','S','F'],np.arange(7,0,-1))
 
-		# convert to timestamps
+		# Obtention du contexte temporel des matchs : année et semaine dans l'année
 		self.dataDF['event_time'] = pd.to_datetime(self.dataDF['event_time'])
-
-		# create week
 		self.dataDF['event_week'] = self.dataDF['event_time'].apply(lambda x : x.week)
-
-		# create year
 		self.dataDF['event_year'] = self.dataDF['event_time'].apply(lambda x : x.year)
 
-
-		# for player a and b
+		# Features concernant les joueurs indépendamment l'un de l'autre
 		for prefix in ['a','b']:
 
-			# create age 
+			# Age des joueurs au moment des matchs (en années)
 			self.dataDF[prefix + '_age'] = self.dataDF[prefix + '_age'].apply(lambda x: x[-11:-1] if not pd.isnull(x) else x)
 			self.dataDF[prefix + '_age'] =  pd.to_datetime(self.dataDF[prefix + '_age'],format='%d.%m.%Y')
 			self.dataDF[prefix + '_age'] = (self.dataDF['event_time'] - self.dataDF[prefix + '_age']).astype('timedelta64[Y]') 
 
-
-			# create imc 
+			# IMC des joueurs à partir de leur taille et poids
 			self.dataDF[prefix + '_imc'] = self.dataDF[prefix + '_weight'].astype(float) / (self.dataDF[prefix + '_height'].astype(float)/100)**2
 
-			# clean hand
+			# Main du joueur (droitier/gaucher/ambidextre/inconnu)
 			self.dataDF[prefix + '_plays'] = self.dataDF[prefix + '_plays'].fillna(' Unknown')
 
-			# turned pro
+			# Nombre d'années passées sur le circuit pro au moment des matchs
 			self.dataDF[prefix + '_turned pro'] = self.dataDF[prefix + '_turned pro'].replace(0,np.nan)
 			self.dataDF[prefix + '_turnedpro'] = self.dataDF['event_year'] - self.dataDF[prefix + '_turned pro']
 			self.dataDF.drop([prefix + '_turned pro'],inplace=True,axis=1)
-			self.dataDF[prefix + '_turnedpro'][self.dataDF[prefix + '_turnedpro'] < 1] = 0
+
+			# Cas des joueurs amateurs au moment du match mais devenus pro ensuite => 0 année d'ancienneté
+			self.dataDF.loc[ self.dataDF[prefix + '_turnedpro'] < 1, prefix + '_turnedpro'] = 0
+		
 		return self
 
 
 	def createStatisticalFeatures(self):
 		return self
 
-
-
+	
 	def getAllFeatures(self):
 		return self.dataDF
 
+	
 	def getBasicFeatures(self):
+		
 		basicPlayerFeatures = []
 		for prefix in ['a','b']:
-			for feat in ['age','height','weight','plays','rank','turnedpro','imc']:
-				basicPlayerFeatures.append(prefix+'_'+feat)
+			for feature in self.basicPlayerFeatures :
+				basicPlayerFeatures.append(prefix+'_'+feature)
 
-		basicEventFeatures = ['surface','category','num_of_sets','round','event_week','event_year']
-
-		features = np.r_[basicPlayerFeatures,basicEventFeatures]
+		features = np.r_[basicPlayerFeatures,self.basicEventFeatures]
 		return self.dataDF[features]
 
+	
 	def getLabel(self):
 		return self.dataDF['winner']
 
@@ -82,8 +86,4 @@ if __name__ == '__main__':
 	features =  featuresExtractor()
 	features.readData()
 	features.createBasicFeatures()
-	featuresDF = features.getFeatures()
-
-
-
-
+	featuresDF = features.getAllFeatures()
